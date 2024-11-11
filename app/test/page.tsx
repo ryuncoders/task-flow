4ad37fb5f071f903b2724b309b1e1413b30d9c4b
ryuncoders <seungjy10@gmail.com>
2024-11-11 23:48:58 +0900

    test: 드레그 테스트, 날짜 추가 확인

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,10 +2,29 @@
 
 import { useState } from "react";
 
+const week = [
+  {
+    index: 0,
+    day: 11,
+    date: "월",
+  },
+  {
+    index: 1,
+    day: 12,
+    date: "화",
+  },
+  { index: 2, day: 13, date: "수" },
+  { index: 3, day: 14, date: "목" },
+  { index: 4, day: 15, date: "금" },
+  { index: 5, day: 16, date: "토" },
+  { index: 6, day: 17, date: "일" },
+];
+
 export default function DragColoring() {
   // 각 div의 색상을 저장할 배열
   const [colors, setColors] = useState(Array(7).fill("#ffffff"));
   const [isDragging, setIsDragging] = useState(false);
+  const [taskDate, setTaskDate] = useState([0, 0]);
 
   // 색칠할 색상 (필요에 따라 변경 가능)
   const paintColor = "#ffdd57";
@@ -15,14 +34,16 @@ export default function DragColoring() {
     event.preventDefault();
     setIsDragging(true);
     paintDiv(index);
+    setTaskDate((prev) => [index, prev[1]]);
   };
 
   const handleMouseEnter = (index: number) => {
     if (isDragging) paintDiv(index);
   };
 
-  const handleMouseUp = () => {
+  const handleMouseUp = (index: number) => {
     setIsDragging(false);
+    setTaskDate((prev) => [prev[0], index]);
   };
 
   const paintDiv = (index: number) => {
@@ -35,23 +56,39 @@ export default function DragColoring() {
 
   return (
     <div
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp} // 드래그 중 영역을 벗어나면 드래그 종료
+    // onMouseUp={handleMouseUp}
+    // onMouseLeave={handleMouseUp} // 드래그 중 영역을 벗어나면 드래그 종료
     >
-      {colors.map((color, index) => (
-        <div
-          key={index}
-          style={{
-            width: "50px",
-            height: "50px",
-            backgroundColor: color,
-            border: "1px solid #000",
-            display: "inline-block",
-          }}
-          onMouseDown={(e) => handleMouseDown(e, index)}
-          onMouseEnter={() => handleMouseEnter(index)}
-        ></div>
-      ))}
+      <div className="flex ">
+        {week.map((date, index) => (
+          <div key={index} className="px-2">
+            {date.day}({date.date})
+          </div>
+        ))}
+      </div>
+      <>
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            style={{
+              width: "60px",
+              height: "50px",
+              backgroundColor: color,
+              border: "1px solid #000",
+              display: "inline-block",
+            }}
+            onMouseDown={(e) => handleMouseDown(e, index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseUp={() => handleMouseUp(index)}
+          ></div>
+        ))}
+      </>
+      <div>
+        <h1>isDragging</h1>
+        <div>taskDate: {taskDate}</div>
+        <span>start Date: {week[Math.min(...taskDate)].day} / </span>
+        <span>end Date: {week[Math.max(...taskDate)].day}</span>
+      </div>
     </div>
   );
 }
