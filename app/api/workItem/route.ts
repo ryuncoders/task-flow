b17f1d5790ff6719c995f4e6386c2b72f1f53700
ryuncoders <seungjy10@gmail.com>
2024-11-28 11:59:21 +0900

    feat: workItem 실제 값 렌더링 및 관련 기능 추가
    - workItem 데이터를 렌더링하기 위한 GET API 추가
    - 새로운 timeLine을 그릴 수 있도록 그리드 위치(UI) 변경
    - timeLine을 반대로 드래그할 때 발생하던 오류 수정

--- app/api/workItem/route.ts
+++ app/api/workItem/route.ts
@@ -1,15 +1,40 @@
 import prisma from "@/lib/prisma";
+import { getWeeklyColors } from "@/lib/utils";
+import { IWorkItem } from "@/types/models";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+interface ReturnWorkItem {
+  success: boolean;
+  error?: string;
+  updatedWorkItems?: Array<{
+    id: number;
+    title: string;
+    timeLines: Array<{
+      id: number;
+      title: string;
+      dateStart: string;
+      dateEnd: string;
+      color: string;
+      dateTimeLineColor?: string[];
+      tasks: Array<{
+        id: number;
+        isComplete: boolean;
+        text: string;
+        date: string;
+      }>;
+    }>;
+  }>;
+}
+
+export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const goalId = searchParams.get("goalId");
 
-  if (goalId === null) {
+  if (!goalId || isNaN(Number(goalId))) {
     return NextResponse.json({
       success: false,
-      error: "goalId 없음",
-    });
+      error: "유효하지 않은 goalId",
+    } as ReturnWorkItem);
   }
 
   try {
@@ -20,24 +45,60 @@ export async function GET(request: Request) {
       select: {
         id: true,
         title: true,
-        TimeLine: true,
+        timeLines: {
+          select: {
+            id: true,
+            title: true,
+            dateStart: true,
+            dateEnd: true,
+            color: true,
+            tasks: {
+              select: {
+                id: true,
+                isComplete: true,
+                text: true,
+                date: true,
+              },
+            },
+          },
+        },
       },
     });
 
-    if (!workItems) {
+    if (workItems.length === 0) {
       return NextResponse.json({
         success: false,
-        error: "workItem 없음",
-      });
+        error: "관련 workItem 없음",
+      } as ReturnWorkItem);
     }
+
+    const updatedWorkItems = workItems.map((workItem) => ({
+      ...workItem,
+      timeLines: workItem.timeLines.map((timeLine) => ({
+        ...timeLine,
+        dateStart: timeLine.dateStart.toISOString(), // Date -> string으로 변환
+        dateEnd: timeLine.dateEnd.toISOString(), // Date -> string으로 변환
+        dateTimeLineColor: getWeeklyColors({
+          dateRange: [timeLine.dateStart + "", timeLine.dateEnd + ""],
+          color: timeLine.color,
+        }),
+        tasks: timeLine.tasks.map((task) => ({
+          ...task,
+          date: task.date.toISOString(), // Date -> string으로 변환
+          isComplete: task.isComplete === "true" ? true : false, // string -> boolean 변환
+        })),
+      })),
+    }));
+
     return NextResponse.json({
       success: true,
-      workItems,
-    });
-  } catch {
+      updatedWorkItems,
+    } as ReturnWorkItem);
+  } catch (error) {
+    console.error("Database query error:", error);
     return NextResponse.json({
       success: false,
-      error: "api 접속 오류",
-    });
+      error: "서버 오류 발생",
+    } as ReturnWorkItem);
   }
 }
--- app/goal/[id]/page.tsx
+++ app/goal/[id]/page.tsx
@@ -1,123 +1,19 @@
 "use client";
 
 import { useItemContext } from "@/contexts/item-context";
-import { getWeekDateWithWeekdays, getWeeklyColors } from "@/lib/utils";
+import {
+  convertToRGBA,
+  getWeekDateWithWeekdays,
+  getWeeklyColors,
+} from "@/lib/utils";
 import { IWorkItem } from "@/types/models";
 import { useParams, useRouter } from "next/navigation";
 
 import { useEffect, useState } from "react";
 
-const test_workItems: IWorkItem[] = [
-  {
-    id: 1,
-    title: "기출풀기",
-    timeLines: [
-      {
-        id: 1,
-        title: "1단원",
-        tasks: [
-          {
-            id: 1,
-            text: "1회 문제풀이",
-            complete: "complete",
-            date: "2024-11-19",
-          },
-          {
-            id: 2,
-            text: "2회 문제풀이",
-            complete: "incomplete",
-            date: "2024-11-20",
-          },
-        ],
-        dateRange: ["2024-11-19", "2024-11-21"],
-        color: "blue",
-        dateTimeLineColor: getWeeklyColors({
-          dateRange: ["2024-11-19", "2024-11-21"],
-          color: "blue",
-        }),
-      },
-      {
-        id: 2,
-        title: "2단원",
-        tasks: [
-          {
-            id: 3,
-            text: "1회 문제풀이",
-            complete: "incomplete",
-            date: "2024-11-21",
-          },
-          {
-            id: 4,
-            text: "2회 문제풀이",
-            complete: "failed",
-            date: "2024-11-23",
-          },
-        ],
-        dateRange: ["2024-11-21", "2024-11-23"],
-        dateTimeLineColor: getWeeklyColors({
-          dateRange: ["2024-11-21", "2024-11-23"],
-          color: "tomato",
-        }),
-        color: "tomato",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "영어 단어 암기",
-    timeLines: [
-      {
-        id: 3,
-        title: "Day 1",
-        tasks: [
-          {
-            id: 5,
-            text: "단어 1~50",
-            complete: "complete",
-            date: "2024-11-19",
-          },
-          {
-            id: 6,
-            text: "단어 복습",
-            complete: "complete",
-            date: "2024-11-20",
-          },
-        ],
-        dateRange: ["2024-11-19", "2024-11-20"],
-        dateTimeLineColor: getWeeklyColors({
-          dateRange: ["2024-11-19", "2024-11-20"],
-          color: "grey",
-        }),
-        color: "grey",
-      },
-      {
-        id: 4,
-        title: "Day 2",
-        tasks: [
-          {
-            id: 7,
-            text: "단어 51~100",
-            complete: "incomplete",
-            date: "2024-11-21",
-          },
-        ],
-        dateRange: ["2024-11-21", "2024-11-21"],
-        dateTimeLineColor: getWeeklyColors({
-          dateRange: ["2024-11-21", "2024-11-21"],
-          color: "pink",
-        }),
-        color: "pink",
-      },
-    ],
-  },
-];
+const paintColor = "black";
 
-const paintColor = "blue";
-
-const defaultTimeLine = [
-  ["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"],
-  ["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"],
-];
+const defaultTimeLine = Array.from(Array(3), () => Array(7).fill("#ffffff"));
 
 export default function GoalPage() {
   const { workItems, setWorkItems } = useItemContext();
@@ -138,7 +34,7 @@ export default function GoalPage() {
         }).then((res) => res.json());
 
         if (response.success) {
-          return response.workItems;
+          setWorkItems(response.updatedWorkItems);
         } else {
           console.log("응답 받지 못함.", response.error);
         }
@@ -147,9 +43,16 @@ export default function GoalPage() {
         setErrorMessage("잘못된 goal 입니다.");
       }
     };
-    const data = fetchWorkItem();
-    setWorkItems(test_workItems);
-  }, []); // 컴포넌트 렌더링 후 한번 실행
+    fetchWorkItem();
+  }, [params.id]); // 컴포넌트 렌더링 후 한번 실행
+
+  useEffect(() => {
+    if (workItems.length > 0) {
+      setGridTimeLine(
+        Array.from(Array(workItems.length), () => Array(7).fill("#ffffff"))
+      );
+    }
+  }, [workItems]);
 
   const weekDateWidthWeekdays = getWeekDateWithWeekdays();
 
@@ -162,7 +65,7 @@ export default function GoalPage() {
     event.preventDefault();
     setIsDragging(true);
     paintDiv(index, color_index);
-    setNewTimeLineIndexAndDate((prev) => [index, color_index, prev[2]]);
+    setNewTimeLineIndexAndDate((prev) => [index, color_index, color_index]);
   };
   const handleMouseEnter = (index: number, color_index: number) => {
     if (isDragging) {
@@ -175,11 +78,10 @@ export default function GoalPage() {
   const handleMouseUp = (index: number, color_index: number) => {
     setIsDragging(false);
 
-    const workItemIndex = newTimeLineIndexAndDate[0];
-    const dateStart = newTimeLineIndexAndDate[1];
-    const dateEnd = newTimeLineIndexAndDate[2];
+    const [workItemIndex, ...date] = newTimeLineIndexAndDate;
 
-    console.log(dateStart, dateEnd);
+    const dateStart = Math.min(...date);
+    const dateEnd = Math.max(...date);
 
     router.push(
       `/goal/${params.id}/task/add?workItemIndex=${workItemIndex}&dateStart=${dateStart}&dateEnd=${dateEnd}`
@@ -217,12 +119,30 @@ export default function GoalPage() {
         </div>
       </div>
 
-      {workItems.map((workItem) => (
-        <div key={workItem.id} className="grid grid-cols-[2fr_7fr]">
+      {workItems.map((workItem, workItem_index) => (
+        <div key={workItem.id} className="grid grid-cols-[2fr_7fr] relative">
           <h1 className="h-[40px]  ">{workItem.title}</h1>
           <div className="relative ">
-            {workItem.timeLines.map((timeLine, idx) => (
-              <div className=" flex  absolute   top-0" key={idx}>
+            {/* gridTimeLine */}
+            <div className="absoulte flex top-0 left-0">
+              {gridTimeLine[workItem_index].map((color, index) => (
+                <div className="border border-black h-[40px] w-20 " key={index}>
+                  <div
+                    className="opacity-50 w-[100%] h-[100%] z-10"
+                    style={{
+                      backgroundColor: color,
+                    }}
+                    onMouseUp={() => handleMouseUp(workItem_index, index)}
+                    onMouseDown={(e) =>
+                      handleMouseDown(e, workItem_index, index)
+                    }
+                    onMouseEnter={() => handleMouseEnter(workItem_index, index)}
+                  />
+                </div>
+              ))}
+            </div>
+            {workItem.timeLines.map((timeLine, index) => (
+              <div className=" flex  absolute   top-0 left-0" key={index}>
                 {/* dateTimeLineColor가 null이면 안됨 */}
                 {timeLine.dateTimeLineColor!.map((c, color_idx) => (
                   <div
@@ -235,27 +155,6 @@ export default function GoalPage() {
                   />
                 ))}
               </div>
-            ))}
-          </div>
-        </div>
-      ))}
-
-      {gridTimeLine.map((line, index) => (
-        <div className="grid grid-cols-[2fr_7fr]" key={index}>
-          <div />
-          <div
-            // onMouseLeave={handleMouseUp(index, color_index)}
-            className="flex"
-          >
-            {[...line].map((color, color_index) => (
-              <div
-                key={color_index}
-                className="border w-20 h-[40px]"
-                style={{ backgroundColor: color }}
-                onMouseUp={() => handleMouseUp(index, color_index)}
-                onMouseDown={(e) => handleMouseDown(e, index, color_index)}
-                onMouseEnter={() => handleMouseEnter(index, color_index)}
-              />
             ))}
           </div>
         </div>
